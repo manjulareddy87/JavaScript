522d47970f2adad673cdf7876f7da123534c38f2
Manjula Karnati <kathamanjula@gmail.com>
2020-07-07 23:08:22 -0700

    Adding New Files

---
+++ classes.js
@@ -0,0 +1,11 @@
+class Employee {
+  setDatails(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+let emp1 = new Employee();
+emp1.setDatails("John", 1001);
+console.log(emp1.name);
+console.log;
---
+++ functions.js
@@ -0,0 +1,14 @@
+// function function (param1, param2){
+//statments
+//}
+
+function add(a, b) {
+  //console.log (a + b);
+  return a + b;
+}
+
+let result = add(2, 4);
+console.log(result);
+
+add(2, 4);
+add(1, 3);
